a7cabf327cc20bcf8aab20848543f8d047107a07
Uyen Nguyen <thaouyen.nttu@gmail.com>
2025-08-08 02:00:32 -0400

    Remove assignment-2-video-search/2-detect-objects/download-custom-img folder

--- assignment-2-video-search/2-detect-objects/download-custom-img/download-custom-img.py
+++
@@ -1,7 +0,0 @@
-from simple_image_download import simple_image_download as sid
-
-response = sid.simple_image_download
-# keywords = ["buildings", "bridge", "lakes", "ocean", "sewage", "leaf", "tree"]
-keywords = ["plant grass", "flower"]
-for kw in keywords:
-    response().download(kw, limit=200)
